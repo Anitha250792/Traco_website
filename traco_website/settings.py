81e0fd7f2e6c84cd58106e3886ccd284ba02701c
Anitha250792 <ntanithasaravanan@gmail.com>
2025-09-11 06:59:06 +0530

    Update settings.py

--- traco_website/settings.py
+++ traco_website/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'muppuraj11@gmail.com'   # your Gmail
+EMAIL_HOST_USER = 'ntanithasaravanan@gmail.com'   # your Gmail
 EMAIL_HOST_PASSWORD = 'dnod msdd thvd suxb'  # app password, not Gmail login
